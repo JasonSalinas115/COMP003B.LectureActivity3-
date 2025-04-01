7d670d30e35786d4c2a25fb178dce7011dd52646
Jason Salinas <jasonsalinas2@my.whccd.edu>
2025-03-31 18:38:30 -0700

    Added controller ProductController

---
+++ COMP003B.LectureActivity3-/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.LectureActivity3-.Models;
+
+namespace COMP003B.LectureActivity3_.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromForm] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            return RedirectToAction("Success", product);
+        }
+
+        [Route("success")]
+        public IActionResult success(Product product)
+        {
+            return View(product);
+        }
+    }
+}
