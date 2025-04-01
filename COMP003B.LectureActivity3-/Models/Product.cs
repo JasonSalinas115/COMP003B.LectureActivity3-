644ca879beb3a79b4cdea4bc329192eaadfb96d8
Jason Salinas <jasonsalinas2@my.whccd.edu>
2025-03-31 18:31:28 -0700

    Add project files.

---
+++ COMP003B.LectureActivity3-/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace COMP003B.LectureActivity3_.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+
+        public string Name { get; set; }
+
+        [Range(0.01, 10000)]
+        public decimal Price { get; set; }
+
+        [Requried]
+        public string Category { get; set; }
+    }
+}
